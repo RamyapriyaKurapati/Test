545be2fde23dae6548b44b700b795bbb9b4ae68e
RamyapriyaKurapati <ramyapriya.kurapati@netenrich.com>
2018-11-01 15:03:35 +0530

    todolist

---
+++ todolist/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+var tododata;
+var dataList=[];
+document.getElementById("input1").addEventListener("change",e =>{
+    tododata=e.target.value
+})
+document.getElementById("button1").addEventListener("click",e=>{
+    dataList.unshift(tododata);
+    var li = document.createElement("li");
+    var node = document.createTextNode(tododata);
+    li.appendChild(node);
+    var list=document.getElementById("ul")
+    if(tododata.trim()!==""){
+       //document.getElementById("ul").appendChild(li);
+       list.insertBefore(li, list.childNodes[0]);
+    }
+    document.getElementById("input1").value="" 
+    tododata=""
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    var close = document.getElementsByClassName("close");
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+    
+})
+
+app.initialize();
